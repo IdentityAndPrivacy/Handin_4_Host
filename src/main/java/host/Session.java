f26b3699ca572a3ed9e9a07c2c8273bdfb15cc79
Martin Jensen <martin2610@gmail.com>
2015-08-06 12:41:41 +0200

    Hello at /session

---
+++ src/main/java/host/Session.java
@@ -0,0 +1,20 @@
+package host;
+
+public class Session {
+
+    private final long id;
+    private final String content;
+
+    public Session(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
